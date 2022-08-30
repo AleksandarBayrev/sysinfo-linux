d8614669286590c255af13b6e8bf0429f76b9873
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-08-30 19:56:51 +0300

    Update .gitignore

--- build/frontend/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const getData = () => {
-    return fetch("/sensors").then(x => x.json()).then((x) => {
-        return x.sensorsResult.split("\n").map(x => x);
-    });
-};
-window.getMachineData = getData;
